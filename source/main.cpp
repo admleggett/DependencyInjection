cb80101011bbfa50127499e5b027a22140dcc45b
Adam Leggett <admleggett@gmail.com>
2024-10-23 13:36:31 +0100

    Injection of repository using main app

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include "Repository/PersonRepository.h"
+#include "Service/PersonService.h"
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Contacts" << std::endl;
+
+    //create a concrete PersonRepository
+    PersonRepository personRepository = PersonRepository();
+    //inject into the service via ctor
+    PersonService personService = PersonService(personRepository);
+    //get the list of persons from the service
+    std::vector<Person> persons = personService.getAllPersons();
+    //print them out
+    for (Person person : persons) {
+        std::cout << person.toString() << std::endl;
+    }
+
     return 0;
 }
